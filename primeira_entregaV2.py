093c05b297f0104ce99bcbe9a5870d0b44930ce9
Erick Mateus <mateusantosbragas@gmail.com>
2026-06-20 20:09:44 -0300

    Update primeira_entregaV2.py

--- primeira_entregaV2.py
+++ primeira_entregaV2.py
@@ -1,57 +1,63 @@
 from tkinter import *
 from tkinter import ttk 
 from tkinter import colorchooser
+from fig import *
 
-
+#Aqui a função define a classe(tipo da figura)
 def iniciar_figura(event):
-    global x1, y1
-    x1 = event.x
-    y1 = event.y
-    
-def atualizar_figura(event):
-    global x1, x2, y1, y2, figura_nova
-    x2, y2 = event.x, event.y
-    if(figura_nova):
-        canvas.delete(figura_nova)
-    figura_nova = desenhar()
-    
-
-def terminar_figura(event):
-    global figura_nova
-    figura_nova = None
-    
-def desenhar():
-    global x1, y1, x2, y2, figura_nova, cor_carregada, cor_preenchimento
+    global x1, y1, x2, y2, figura_nova, cor_linha, cor_fundo
+    x1,y1,x2,y2 = event.x,event.y,event.x,event.y
     figura = tipo_figura.get()
     if figura == "Retângulo":
-        return canvas.create_rectangle(x1,y1,x2,y2,outline=cor_carregada,fill=cor_preenchimento)
+        figura_nova = Retangulo(x1,y1,x2,y2,cor_linha,cor_fundo)
     elif figura == "Oval":
-        return canvas.create_oval(x1,y1,x2,y2,outline=cor_carregada,fill=cor_preenchimento)
+        figura_nova = Oval(x1,y1,x2,y2,cor_linha,cor_fundo)
     elif figura == "Linha":
-        return canvas.create_line(x1,y1,x2,y2,fill=cor_carregada)
+        figura_nova = Linha(x1,y1,x2,y2,cor_linha)
     elif figura == "Rabisco":
-        canvas.create_line(x1,y1,x2,y2, fill=cor_carregada)
-        x1,y1 = x2,y2
+        figura_nova = Rabisco(x1,y1,cor_linha)
     elif figura == "Círculo":
-        r = ((x2 - x1) **2 + (y2 - y1)**2)**0.5
-        return canvas.create_oval(x1 - r, y1 - r, x1 + r, y1 + r,outline=cor_carregada,fill=cor_preenchimento)
+        figura_nova = Circulo(x1,y1,x2,y2,cor_linha,cor_fundo)
+        
+#Aqui é atualizado o segundo ponto,enquanto desenha a figura atual e armazenadas
+def atualizar_figura(event):
+    global figura_nova
 
-cor_carregada = "Black"
+    figura_nova.atualizar(event.x,event.y)
+    desenhar()  
+    figura_nova.desenhar(canvas)  
+    
+#Aqui é armazenada a figura atual
+def terminar_figura(event):
+    global figuras
+    figuras.append(figura_nova)
+    
+#Aqui são desenhadas as figuras armazenadas 
+def desenhar():
+    global figuras
+    canvas.delete("all")
+    for figura in figuras:
+        figura.desenhar(canvas)
+    
+cor_linha = None
 def escolher_Cor_borda(): 
-    global cor_carregada
+    global cor_linha
     cor = colorchooser.askcolor(title="Selecionar Cor")
     if cor and cor[1]:
-        cor_carregada = cor[1]
+        cor_linha = cor[1]
 
-cor_preenchimento = None
+cor_fundo = None
 def escolher_Cor_preenchimento(): 
-    global cor_preenchimento
+    global cor_fundo
     cor = colorchooser.askcolor(title="Selecionar Cor para preencher")
     if cor and cor[1]:
-        cor_preenchimento = cor[1]
+        cor_fundo = cor[1]
 
 def deletar():
+    global figuras,cor_linha,cor_fundo
+    cor_linha, cor_fundo = None, None
     canvas.delete("all")
+    figuras = []
 
 x1 = None
 y1 = None
@@ -72,10 +78,10 @@ frame.pack(fill=X)
 
 #Criação do Espaço Canvas
 canvas = Canvas(janela, width= 600, height= 600,bg ="white" )
-canvas.pack(pady=10, padx= 10)
+canvas.pack(pady=10, padx= 10,fill=BOTH)
 
 #Botao de Apagar
-apagar = Button(frame, text="Apagar", command=deletar)
+apagar = Button(frame, text="Resetar", command=deletar)
 apagar.pack(side=TOP)
 
 #Interface de cores
